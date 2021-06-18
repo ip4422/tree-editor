f8880de7192cecba5e8c2130e1d6ff0856235056
ipopov <ipopov4422@gmail.com>
2021-06-18 20:44:43 +0300

    Craco scripts added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,20 @@
 import React from 'react'
+import './App.less'
+import { Layout } from 'antd'
 
 export function App() {
-  return <div>Application</div>
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Layout.Header style={{ color: 'white'}}>Header</Layout.Header>
+      <Layout.Content style={{ padding: '20px 50px' }}>
+        content
+        {/* <ApplicationRoutes routes={ROUTES} /> */}
+      </Layout.Content>
+      <Layout.Footer
+        style={{ textAlign: 'center', position: 'sticky', bottom: '0' }}
+      >
+        Application for displaying Github user's public repositories
+      </Layout.Footer>
+    </Layout>
+  )
 }
