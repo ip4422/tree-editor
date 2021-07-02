3078b9273ac60ee4c522171a341bcbd7b3f12faf
Igor Popov <ipopov4422@gmail.com>
2021-07-02 13:13:25 +0300

    Build for GitHub pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './app'
 
@@ -9,9 +8,7 @@ import { App } from './app'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </Provider>,
   document.getElementById('root')
 )
